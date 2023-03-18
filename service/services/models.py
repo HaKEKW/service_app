e7c6de2fd4ed6b08edc3a08b23415b3d3a234a65
Hak <art.petr0213@gmail.com>
2023-03-18 23:10:21 +0300

    updated docker-compose, added models to apps

---
+++ service/services/models.py
@@ -0,0 +1,33 @@
+from django.core.validators import MaxValueValidator
+from django.db import models
+
+from clients.models import Client
+
+
+# Create your models here.
+
+class Service(models.Model):
+    name = models.CharField(max_length=50)
+    full_price = models.PositiveIntegerField()
+
+
+class Plan(models.Model):
+    PLAN_TYPES = (
+        ('FULL', 'FULL'),
+        ('STUDENT', 'STUDENT'),
+        ('DISCOUNT', 'DISCOUNT'),
+    )
+    plan_type = models.CharField(choices=PLAN_TYPES, max_length=10)
+    discount_percent = models.PositiveIntegerField(default=0, validators=[
+        MaxValueValidator(100)
+    ])
+
+    def __str__(self):
+        return self.plan_type
+
+
+class Subscription(models.Model):
+    client = models.ForeignKey(Client, related_name='subscription', on_delete=models.PROTECT)
+    service = models.ForeignKey(Service, related_name='subscription', on_delete=models.PROTECT)
+    plan = models.ForeignKey(Plan, related_name='subscription', on_delete=models.PROTECT)
+
